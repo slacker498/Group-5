e9f6984ead5edc86db998bbae86549cadb3b5088
slacker498 <jachinkpogli@gmail.com>
2025-09-25 12:48:19 +0000

    add file

--- src/Question7.java
+++ src/Question7.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+
 public class Question7 {
-    
+    public static void main(String[] args) {
+        String course;
+        int year;
+        String major = "";
+        String x = "";
+        System.out.println("enter your course: M,C ,or I");
+        Scanner input = new Scanner(System.in);
+        course = input.nextLine();
+        System.out.println("enter your year: 1,2,3,4");
+        year = input.nextInt();
+        switch (course) {
+            case "M":
+                major = "Mathematics";
+                break;
+            case "C":
+                major = "Computer Science";
+                break;
+            case "I":
+                major = "Information Technology";
+
+
+        }
+        switch (year) {
+            case 1:
+                x = "Freshman";
+                break;
+            case 2:
+                x = "Sophomore";
+                break;
+            case 3:
+                x = "Junior";
+                break;
+            case 4:
+                x = "Senior";
+                break;
+    }
+    System.out.println("You are a " + major + x);
+}
 }
