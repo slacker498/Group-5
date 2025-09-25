1070333d5e75b12ab6d643f4b4f1817101483d6a
slacker498 <111001617+slacker498@users.noreply.github.com>
2025-09-25 12:51:49 +0000

    Update Question3.java

--- src/Question3.java
+++ src/Question3.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the letter you want to convert:");
+        String letter = sc.next();
+
+        switch (letter.toLowerCase()) {
+            case "a", "b","c": 
+            System.out.println("your number is 2"); break;
+            
+            case "d", "e","f": 
+            System.out.println("your number is 3"); break;
+            case "g", "h","i": 
+            System.out.println("your number is 4"); break;
+            case "j", "k","l": 
+            System.out.println("your number is 5"); break;
+            case "m", "n","o": 
+            System.out.println("your number is 6"); break;
+            case "r", "q","p","s": 
+            System.out.println("your number is 7"); break;
+            case "v", "t","u": 
+            System.out.println("your number is 8"); break;
+            case "z", "w","x","y": 
+            System.out.println("your number is 9"); break;
+            
+            default:
+            System.out.println("Enter a valid letter!"); 
+                break;
+        }
+
+    }
+}
